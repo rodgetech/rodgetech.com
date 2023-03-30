ff590b2bc42b87e12f8a479b0b03bf8d0707f08c
Luis Rodriguez <luisrodge@outlook.com>
2023-03-29 20:50:19 -0600

    setup react

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,10 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://rodgetech.com",
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), react()]
 });
---
+++ src/components/Chat.tsx
@@ -0,0 +1,9 @@
+export default function Chat() {
+  return (
+    <div className="flex flex-col justify-between gap-y-2 bg-white/[0.03] border border-white/20  p-4 rounded-lg">
+      <h2 className="text-2xl text-neutral-400">
+        Something awesome coming here soon
+      </h2>
+    </div>
+  );
+}
